d0bac8fad4975d3ccd88006901093cb88099afb4
WaleedTariq109 <waleedtariq109@outlook.com>
2022-02-14 03:52:54 +0500

    render questions

---
+++ 2-quiz-app/src/components/Questions.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div class="bar"></div>
+      <div class="status">1 out of 3 questions answered</div>
+    </div>
+    <div
+      class="single-question"
+      v-for="(question, index) in questions"
+      :key="question.q"
+      v-show="questionsAnswered === index"
+    >
+      <div class="question">{{ question.q }}</div>
+      <div class="answers">
+        <div
+          class="answer"
+          v-for="answer in question.answers"
+          :key="answer.text"
+        >
+          {{ answer.text }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Question",
+  props: {
+    questions: {
+      type: Array,
+      required: true,
+    },
+    questionsAnswered: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
