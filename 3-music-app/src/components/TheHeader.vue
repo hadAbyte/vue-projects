4e78d8139555aa62edb7789cc9150f948c7a9ca6
Waleed Tariq <waleedtariq109@outlook.com>
2022-02-26 05:56:06 +0500

    Add routes

--- 3-music-app/src/components/TheHeader.vue
+++ 3-music-app/src/components/TheHeader.vue
@@ -3,12 +3,21 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        exact-active-class="no-active"
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+      >
+        Music
+      </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
+          </li>
           <li v-if="!userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
@@ -16,7 +25,7 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="signout">Logout</a>
@@ -29,7 +38,7 @@
 </template>
 
 <script>
-import { mapMutations, mapState, mapActions } from "vuex";
+import { mapMutations, mapState } from "vuex";
 
 export default {
   name: "Header",
@@ -38,7 +47,13 @@ export default {
   },
   methods: {
     ...mapMutations(["toggleAuthModal"]),
-    ...mapActions(["signout"]),
+
+    signout() {
+      this.$store.dispatch("signout");
+      if (this.$route.name === "manage") {
+        this.$router.push({ name: "home" });
+      }
+    },
   },
 };
 </script>
--- 3-music-app/src/router/index.js
+++ 3-music-app/src/router/index.js
@@ -1,25 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView,
+    name: "home",
+    path: "/",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    name: "about",
+    path: "/about",
+    component: About,
+  },
+  {
+    name: "manage",
+    path: "/manage-music",
+    component: Manage,
+  },
+  {
+    path: "/manage",
+    redirect: { name: "manage" },
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: "text-yellow-500",
 });
 
 export default router;
