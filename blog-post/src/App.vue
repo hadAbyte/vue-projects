c9d90bac79658cb2cb574ada8f66dba9d542ca33
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-10-30 04:35:59 +0500

    initial commit

---
+++ blog-post/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Timeline from './components/Timeline.vue';
+</script>
+
+<template>
+  <Timeline />
+</template>
+
+<style>
+@import 'https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css';
+</style>
