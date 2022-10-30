76869b57e4165bc08c180c64f68d5afd6a20e2d9
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-10-30 06:02:07 +0500

    add navbar

---
+++ blog-post/src/components/Timeline.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import {ref} from "vue";
+
+const periods = <const>['Today', 'This Week', 'This Month']
+
+type Period = typeof periods[number]
+
+const selectedPeriod = ref<Period>('Today')
+
+function selectPeriod(period: Period) {
+	selectedPeriod.value = period
+}
+</script>
+
+<template>
+	<nav class="is-primary panel">
+		<span class="panel-tabs">
+			<a
+					v-for="period in periods"
+					:key="period"
+					:class="{
+						'is-active': period === selectedPeriod
+					}"
+					@click="selectPeriod(period)"
+			>
+				{{ period }}
+			</a>
+		</span>
+	</nav>
+</template>
