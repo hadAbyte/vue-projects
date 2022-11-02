cbae3fcce8740159d7eff2f940026568aa5b2e67
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-11-03 00:16:08 +0500

    lint: format code

--- blog-post/src/components/Timeline.vue
+++ blog-post/src/components/Timeline.vue
@@ -1,57 +1,58 @@
 <script lang="ts" setup>
-import {computed, onMounted, ref} from "vue";
-import {thisMonth, today} from "../post";
-import {DateTime} from "luxon";
+import { computed, ref } from 'vue';
+import { thisMonth, today } from '../post';
+import { DateTime } from 'luxon';
 
-const periods = <const>['Today', 'This Week', 'This Month']
+const periods = <const>['Today', 'This Week', 'This Month'];
 
-type Period = typeof periods[number]
+type Period = typeof periods[number];
 
-const selectedPeriod = ref<Period>('Today')
+const selectedPeriod = ref<Period>('Today');
 
 function selectPeriod(period: Period) {
-	selectedPeriod.value = period
+  selectedPeriod.value = period;
 }
 
 const posts = computed(() => {
-	return [today, thisMonth, thisMonth].map(post => {
-		return {
-			...post,
-			created: DateTime.fromISO(post.created)
-		}
-	}).filter(post => {
-		if (selectedPeriod.value === 'Today') {
-			return post.created >= DateTime.now().minus({day: 1})
-		}
+  return [today, thisMonth, thisMonth]
+    .map((post) => {
+      return {
+        ...post,
+        created: DateTime.fromISO(post.created),
+      };
+    })
+    .filter((post) => {
+      if (selectedPeriod.value === 'Today') {
+        return post.created >= DateTime.now().minus({ day: 1 });
+      }
 
-		return post
-	})
-})
-
+      return post;
+    });
+});
 </script>
 
 <template>
-	<nav class="is-primary panel">
-		<span class="panel-tabs">
-			<a
-					v-for="period in periods"
-					:key="period"
-					:class="{
-						'is-active': period === selectedPeriod
-					}"
-					@click="selectPeriod(period)"
-			>
-				{{ period }}
-			</a>
-		</span>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a
+        v-for="period in periods"
+        :key="period"
+        :class="{
+          'is-active': period === selectedPeriod,
+        }"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
 
-		<a
-				v-for="post in posts"
-				:key="post"
-				class="panel-block"
-		>
-			<a>{{ post.title }}</a>
-			<div>{{ post.created.toFormat('d MMM') }}</div>
-		</a>
-	</nav>
+    <a
+      v-for="post in posts"
+      :key="`${post}`"
+      class="panel-block"
+    >
+      <a>{{ post.title }}</a>
+      <div>{{ post.created.toFormat('d MMM') }}</div>
+    </a>
+  </nav>
 </template>
